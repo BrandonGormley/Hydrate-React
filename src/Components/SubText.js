c6f76f43d2a471b7f8de5595ddc74f1f9737b80d
Brandon Gormley <brandontgormley@gmail.com>
2021-02-12 14:02:04 -0500

    Changed html

--- src/Components/SubText.js
+++ src/Components/SubText.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function SubText() {
-    return (
-        <div className="subtext">
-            <p>(Calculations based off of your body being 60% water and keeping your body
-            hydrated daily flushing out any toxins.)</p>
-        </div>
-    )
+  return (
+    <div className="subtext">
+      <p>
+        (Calculations based off of your body being 66% water and keeping your
+        body hydrated daily flushing out any toxins.)
+      </p>
+    </div>
+  );
 }
 
-export default SubText
+export default SubText;
