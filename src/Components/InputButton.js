d68a11dd6f6cbe59a48e6e969ac41f6d65bbbb16
Brandon Gormley <brandontgormley@gmail.com>
2021-02-12 14:01:10 -0500

    Changed React Structure
    Changed from class/component to hooks

--- src/Components/InputButton.js
+++ src/Components/InputButton.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class InputButton extends Component {
-    constructor() {
-        super()
-        this.state = {
-            totalWater: 0
-        }
-    }
+const InputButton = () => {
+  const [water, setWater] = useState('');
 
+  return (
+    <React.Fragment>
+      <form>
+        <label htmlFor="lbs"></label>
+        <input
+          type="text"
+          id="lbs"
+          name="lbs"
+          className="main-input"
+          placeholder="100lbs"
+          value={water}
+          onChange={e => {
+            setWater(e.target.value);
+          }}
+        />
+        <p className="water-total">{water * 0.67} fl. ounces</p>
+      </form>
+    </React.Fragment>
+  );
+};
 
-    onChange = e => {
-        const tW = e.target.value * 0.67
-    }
-
-    handleClick = () => {
-        this.setState({
-            totalWater: this.tW
-        })
-        console.log(this.tW)
-    }
-
-    render() {
-        return (
-            <div>
-                <input onChange={this.onChange} className="main-input" type="text" placeholder="100lbs" /> <br />
-                <button onClick={this.handleClick} className="main-button">Drink Water</button>
-                <p className="water-total">{this.state.totalWater} fl. oz</p>
-            </div>
-        )
-    }
-}
-
-export default InputButton
+export default InputButton;
